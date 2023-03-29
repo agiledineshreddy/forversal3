61f3447f3ff8bf2ae56e4a1f2d383e53042619f5
agiledineshreddy <126139844+agiledineshreddy@users.noreply.github.com>
2023-03-29 14:04:57 +0530

    Add files via upload

---
+++ gpt.js
@@ -0,0 +1,18 @@
+function Display(value) {
+	// Append the button value to the display
+	document.getElementById("display").value += value;
+}
+function calculate() {
+	// Evaluate the expression in the display
+	
+	// Display the result
+	document.getElementById("display").value = eval(display.value);
+}
+function dd() {
+	document.getElementById("display").value= '';//display.value.slice(0,-1);
+
+}
+function del() {
+	document.getElementById("display").value= display.value.slice(0,-1);
+
+}
